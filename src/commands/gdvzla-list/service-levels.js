d2c5154a26d0ec337d8f189ff5daa21df1391a4d
Zukaritasu <perezjhonlenno29@gmail.com>
2025-10-11 20:09:13 -0400

    Service in test mode

--- src/commands/gdvzla-list/service-levels.js
+++ src/commands/gdvzla-list/service-levels.js
@@ -1,6 +1,6 @@
 const { Db } = require('mongodb');
 const logger = require('../../logger');
-const { Client } = require('discord.js');
+const { Client, TextChannel } = require('discord.js');
 const GITHUB_TOKEN = require('../../../.botconfig/token.json').GITHUB_TOKEN;
 const aredlapi = require('../../aredlapi');
 const axios = require('axios');
@@ -8,6 +8,38 @@ const { COLL_GDVZLA_LIST_CONFIG } = require('../../../.botconfig/database-info.j
 const channels = require('../../../.botconfig/channels.json');
 
 //////////////////////////////////////
+
+/**
+ * @typedef {Object} ListData
+ * @property {string} sha - The SHA of the list file.
+ * @property {string[]} content - The array of level names in the list.
+ */
+
+/**
+ * @typedef {Object} NormalizedLevel
+ * @property {string} originName - The original name of the level.
+ * @property {string} name - The normalized name of the level.
+ * @property {number} position - The position of the level in the list.
+ */
+
+/**
+ * @typedef {Object} LastChangelog
+ * @property {Object} _id - The document ID.
+ * @property {aredlapi.ChangelogEntry} data - The last changelog entry.
+ */
+
+/**
+ * @typedef {Object} ListChangeEvents
+ * @property {string} levelName - The name of the level that changed.
+ * @property {number} from - The original position of the level.
+ * @property {number} to - The new position of the level.
+ * @property {string|null} above - The name of the level above the new position, if any.
+ * @property {string|null} below - The name of the level below the new position, if any.
+ * @property {string|null} pushed - The name of the level that was pushed out of its range, if any.
+ * @property {'main' | 'extended' | 'legacy' | null} pushedTarget - The target range of the pushed level, if any.
+ * @property {string|null} pushed2 - The name of the level that was pushed out of its range, if any.
+ * @property {'main' | 'extended' | 'legacy' | null} pushedTarget2 - The target range of the pushed level, if any.
+ */
 
 const TIME_INTERVAL = 14400000; // 4 hours
 
@@ -17,6 +49,7 @@ const RANK_LIMITS = {
 };
 
 /**
+ * Determines the range of a level based on its position.
  * @param {number} position
  * @returns {'main' | 'extended' | 'legacy'} range
  */
@@ -53,9 +86,10 @@ async function getListLevels() {
         throw new Error(`Failed to fetch GD Venezuela List: ${response.statusText}`);
     }
 
+    const raw = JSON.parse(Buffer.from(response.data.content, "base64").toString());
     return {
         sha: response.data.sha,
-        content: JSON.parse(Buffer.from(response.data.content, "base64").toString())
+        content: Array.isArray(raw) ? raw : Object.values(raw)
     };
 }
 
@@ -89,22 +123,221 @@ async function isListUpdatable(db) {
     return false;
 }
 
-function generateChangeMessage({ name, from, to, above, below, pushed, pushedTarget }) {
+/**
+ * Saves the sorted list to GitHub.
+ * @param {string} sha - The SHA of the current list file.
+ * @param {string[]} sortedList - The sorted list of level names.
+ */
+async function saveSortedList(sha, sortedList) {
+    logger.DBG('Save Sorted List...')
+    await axios.put(`https://api.github.com/repos/Abuigsito/gdvzla/contents/data/_list.json`, {
+        message: `Sorted list _list.json`,
+        content: Buffer.from(JSON.stringify(sortedList, null, 4)).toString('base64'),
+        sha: sha,
+        branch: 'main'
+    }, {
+        headers: {
+            Authorization: `token ${GITHUB_TOKEN}`
+        }
+    });
+    return
+}
+
+/**
+ * Generates a message describing the change in level position.
+ * @param {ListChangeEvents} changeEvents - The details of the level change
+ * @returns {string} The generated message
+ */
+function generateChangeMessage(changeEvents) {
+    const {
+        levelName,
+        from,
+        to,
+        above,
+        below,
+        pushed,
+        pushedTarget,
+        pushed2,
+        pushedTarget2
+    } = changeEvents;
+
     const direction = to < from ? 'subido' : 'bajado';
-    const base = `**${name}** ha sido ${direction} desde el top #${from} al top #${to}`;
+    const base = `**${levelName}** ha ${direction} desde el top #${from} al top #${to}`;
 
     let context = '';
     if (to === 1) {
         context = ', convirtiéndose en el nuevo líder de la lista';
     } else if (above && below) {
-        context = ` por encima de ${above} y por debajo de ${below}`;
+        context = `, por encima de **${below}** y por debajo de **${above}**`;
+    } else if (above) {
+        context = `, por debajo de **${above}**`;
     } else if (below) {
-        context = ` por debajo de ${below}`;
+        context = `, por encima de **${below}**`;
     }
 
-    const thrust = pushed ? `. Este cambio empuja a ${pushed} a la ${pushedTarget} list` : '';
+    let thrust = '';
+    if (pushed) {
+        thrust += `. Este cambio ha empujado a **${pushed}** hacia la ${pushedTarget} list`;
+    }
+    if (pushed2) {
+        thrust += ` y también a **${pushed2}** hacia la ${pushedTarget2} list`;
+    }
 
-    return `${base}${context}${thrust}`;
+    return `${base}${context}${thrust}.`;
+}
+
+/**
+ * Check if the two lists have the same length and if the order of their
+ * contents is identical.
+ * @param {Client} client 
+ * @param {ListData} listLevels 
+ * @param {NormalizedLevel[]} sortedList 
+ * @returns {Promise<boolean>} true if both lists are identical (same elements in the same order), false otherwise
+ */
+async function isSortedList(client, listLevels, sortedList) {
+    if (listLevels.content.length !== sortedList.length) {
+        const channel = await client.channels.fetch(channels.SUBMITS);
+        if (channel && channel.isTextBased())
+            await channel.send('Ha ocurrido un error al ordenar la lista de niveles de la **GD Venezuela List**.\nPosibles causas:\n\n- Hay más de un nivel con el mismo nombre\n- Se ha eliminado un nivel\n- Error interno en la **API** de la **AREDL**');
+        throw new Error('The number of levels in the GD Venezuela List does not match the number of levels fetched from the API');
+    }
+
+    for (let i = 0; i < listLevels.content.length; i++) {
+        if (listLevels.content[i] !== sortedList[i].name) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * Extracts events from a changelog entry related to level changes.
+ * @param {aredlapi.ChangelogEntry} change - The changelog entry.
+ * @param {NormalizedLevel[]} normalizedLevels - The list of normalized levels.
+ * @param {ListData} oldNormalizedLevels - Data from the unsorted list (original).
+ * @returns {ListChangeEvents|null} An object containing event details or null if no relevant event is found.
+ */
+function getEventsFromLevelChange(change, normalizedLevels, oldNormalizedLevels) {
+    let levelIndex = normalizedLevels.findIndex(level => level.originName === change.affected_level.name);
+    const events = {
+        levelName: change.affected_level.name,
+        from: oldNormalizedLevels.content.findIndex(name => name === getLevelName(change.affected_level.name)) + 1,
+        to: levelIndex + 1,
+        above: normalizedLevels[levelIndex - 1] ? normalizedLevels[levelIndex - 1].originName : null,
+        below: normalizedLevels[levelIndex + 1] ? normalizedLevels[levelIndex + 1].originName : null,
+        pushed: null,
+        pushedTarget: null,
+        // There is a small chance that a level will move from legacy to main or vice versa,
+        // resulting in two records of levels that have changed list rank
+        pushed2: null,
+        pushed2Target: null
+    }
+
+    // The level that has changed range in the list due to the movement of
+    // the affected level before the list normalization is extracted
+    if (events.from > 75 && events.to <= 75 && events.from <= 150) {
+        events.pushed = normalizedLevels.find(level => oldNormalizedLevels.content[74] == level.name).originName;
+        events.pushedTarget = 'extended';
+    } else if (events.from <= 75 && events.to > 75 && events.to <= 150) {
+        events.pushed = normalizedLevels.find(level => oldNormalizedLevels.content[75] == level.name).originName;
+        events.pushedTarget = 'main';
+    } else if (events.from > 150 && events.to <= 150 && events.to > 75) {
+        events.pushed = normalizedLevels.find(level => oldNormalizedLevels.content[149] == level.name).originName;
+        events.pushedTarget = 'legacy';
+    } else if (events.from <= 150 && events.from > 75 && events.to > 150) {
+        events.pushed = normalizedLevels.find(level => oldNormalizedLevels.content[150] == level.name).originName;
+        events.pushedTarget = 'extended';
+    } else if (events.from > 150 && events.to <= 75) {
+        events.pushed = normalizedLevels.find(level => oldNormalizedLevels.content[74] == level.name).originName;
+        events.pushedTarget = 'extended';
+        events.pushed2 = normalizedLevels.find(level => oldNormalizedLevels.content[149] == level.name).originName;
+        events.pushed2Target = 'legacy';
+    } else if (events.from <= 75 && events.to > 150) {
+        events.pushed = normalizedLevels.find(level => oldNormalizedLevels.content[75] == level.name).originName;
+        events.pushedTarget = 'main';
+        events.pushed2 = normalizedLevels.find(level => oldNormalizedLevels.content[150] == level.name).originName;
+        events.pushed2Target = 'extended';
+    }
+
+    return events
+}
+
+/**
+ * Filters the changelog entries to include only those that affect levels present in the current list.
+ * @param {aredlapi.ChangelogEntry[]} changelog - The full changelog entries.
+ * @param {LastChangelog} doc - The last changelog document from the database.
+ * @param {NormalizedLevel[]} normalizedLevels - The list of normalized levels.
+ * @param {ListData} listData - Data from the unsorted list (original).
+ * @returns {aredlapi.ChangelogEntry[]} The filtered changelog entries.
+ */
+function filterChangelogLevels(changelog, doc, normalizedLevels, listData) {
+    const index = doc ? changelog.data.findIndex(entry => entry === doc.data) : changelog.data.length;
+    const filteredLevels = [];
+    changelog.data.slice(0, index).reverse().forEach(entry => {
+        const levelIndex = normalizedLevels.findIndex(level => level.originName === entry.affected_level.name);
+        if (levelIndex !== -1 && listData.content[levelIndex] !== normalizedLevels[levelIndex].name) {
+            filteredLevels.push(entry);
+        }
+    });
+
+    return filteredLevels;
+}
+
+/**
+ * Prints the changelog of the GD Venezuela List.
+ * @param {Db} db - The MongoDB database instance
+ * @param {Client} client - The Discord client
+ * @param {NormalizedLevel[]} normalizedLevels - The list sorted according to the API result
+ * @param {ListData} listData - Data from the unsorted list (original)
+ */
+async function printChangelog(db, client, normalizedLevels, listData) {
+    const changelog = await aredlapi.getChangelog();
+    /** @type {LastChangelog} */
+    const doc = await db.collection(COLL_GDVZLA_LIST_CONFIG).findOne({ type: 'lastChangelog' });
+
+    if (!changelog.data || changelog.data.length === 0 || (doc && doc.data && changelog.data[0] === doc.data))
+        return; // No new changelog entry
+
+    // The first change record is saved to prevent duplicate reports, although
+    // if the isSortedList function returns true, it means that there are no
+    // changes in the list to report.
+    await db.collection(COLL_GDVZLA_LIST_CONFIG).updateOne(
+        { type: 'lastChangelog' },
+        {
+            $set: {
+                data: changelog.data[0]
+            }
+        },
+        { upsert: true }
+    );
+
+    const filteredChangelog = filterChangelogLevels(changelog.data, doc, normalizedLevels, listData);
+    if (filteredChangelog.length === 0)
+        return; // No relevant changelog entries
+
+    const messages = [];
+    for (let i = 0; i < filteredChangelog.length; i++) {
+        const eventsChange = getEventsFromLevelChange(filteredChangelog[i], normalizedLevels, listData);
+        if (eventsChange) {
+            messages.push(generateChangeMessage(eventsChange))
+        }
+    }
+
+    if (messages.length > 0) {
+        logger.DBG('**************************************')
+        messages.forEach(msg => logger.DBG(msg))
+        /*const channel = await client.channels.fetch(channels.SEND_RECORD);
+        if (channel && channel.isTextBased()) {
+            for (let i = messages.length - 1; i >= 0; i--) {
+                const messageSend = await channel.send({ content: messages[i] }).catch(() => null);
+                if (!messageSend)
+                    return
+                await messageSend.react('👍');
+                await messageSend.react('👎');
+            }
+            await channel.send('<@&1376586957735465111>');
+        }*/
+    }
 }
 
 /**
@@ -115,12 +348,12 @@ async function service(db, client) {
     const functionRun = async () => {
         try {
             if (await isListUpdatable(db)) {
-                /** @type {{sha: string, content: string[]}} */
-                const listLevels = await getListLevels();
-
+                logger.DBG('Sorting list...')
+                /** @type {ListData} */
+                const listData = await getListLevels();
                 const currentLevels = await aredlapi.getLevels();
-
-                let updatedLevels = currentLevels.filter(level => listLevels.content.includes(getLevelName(level.name)))
+                /** @type {NormalizedLevel[]} */
+                let normalizedLevels = currentLevels.filter(level => listData.content.includes(getLevelName(level.name)))
                     .map(level => {
                         return {
                             originName: level.name,
@@ -128,94 +361,14 @@ async function service(db, client) {
                             position: level.position
                         }
                     }).sort((a, b) => a.position - b.position);
-                if (updatedLevels.length !== listLevels.content.length) {
-                    throw new Error('The number of levels in the GD Venezuela List does not match the number of levels fetched from the API');
-                }
 
-                const originalPositions = {};
-                listLevels.content.forEach((name, index) => {
-                    originalPositions[name] = index;
-                });
-
-                /** @type {string[]} */
-                const changedLevels = [];
-
-                updatedLevels.forEach((level, newIndex) => {
-                    const oldIndex = originalPositions[level.name];
-                    const delta = oldIndex - newIndex;
-
-                    if (delta !== 0) {
-                        const from = oldIndex + 1;
-                        const to = newIndex + 1;
-                        const oldRange = getRange(from);
-                        const newRange = getRange(to);
-
-                        const above = updatedLevels[to - 2] || null;
-                        const below = updatedLevels[to] || null;
-
-                        let pushed = null;
-                        let pushedTarget = null;
-
-                        if (oldRange !== newRange) {
-                            if (newRange === 'main' && oldRange === 'extended') {
-                                pushed = updatedLevels[RANK_LIMITS.main];
-                                pushedTarget = 'extended';
-                            } else if (newRange === 'extended' && oldRange === 'legacy') {
-                                pushed = updatedLevels[RANK_LIMITS.extended];
-                                pushedTarget = 'legacy';
-                            } else if (newRange === 'legacy' && oldRange === 'extended') {
-                                pushed = updatedLevels[RANK_LIMITS.extended - 1];
-                                pushedTarget = 'extended';
-                            } else if (newRange === 'extended' && oldRange === 'main') {
-                                pushed = updatedLevels[RANK_LIMITS.main - 1];
-                                pushedTarget = 'main';
-                            }
-                        }
-
-                        if (from <= 150 || to <= 150) {
-                            for (let i = 0; i < changedLevels.length; i++) {
-                                if ([level.originName, above?.originName, below?.originName].filter(Boolean)
-                                        .some(name => changedLevels[i].includes(name))) {
-                                    return; // Avoid duplicate messages
-                                }
-                            }
-
-                            changedLevels.push(generateChangeMessage({
-                                name: level.originName, from, to, above: above?.originName,
-                                below: below?.originName, pushed: pushed?.originName, pushedTarget
-                            }));
-                        }
-                    }
-                });
-
-                if (changedLevels.length > 0) {
-                    await axios.put(`https://api.github.com/repos/Abuigsito/gdvzla/contents/data/_list.json`, {
-                    //await axios.put(`https://api.github.com/repos/Zukaritasu/gdvzla/contents/data/_list.json`, {
-                        message: `Sorted list _list.json`,
-                        content: Buffer.from(JSON.stringify(updatedLevels.map(level => level.name), null, 4)).toString('base64'),
-                        sha: listLevels.sha,
-                        branch: 'main'
-                    }, {
-                        headers: {
-                            Authorization: `token ${GITHUB_TOKEN}`
-                        }
-                    });
-
-                    /** @type {import('discord.js').TextChannel} */
-                    //const channel = await client.channels.fetch(channels.BOT_TESTING);
-                    const channel = await client.channels.fetch(channels.LIST_CHANGES);
-                    if (!channel || !channel.isTextBased()) {
-                        throw new Error('The channel to send the GD Venezuela List changes is not text-based or could not be found.');
-                    }
-
-                    for (let i = 0; i < changedLevels.length; i++) {
-                        const messageSend = await channel.send(changedLevels[i]);
-                        await messageSend.react('👍');
-                        await messageSend.react('👎');
-                    }
-
-                    await channel.send('<@&1376586957735465111>');
-                    //await channel.send('<@&1266941869120684032>');
+                // If the list is sorted, it means that one or more levels
+                // of the list have been moved, which means that the changes
+                // will be saved directly to GitHub and notified in the
+                // corresponding channel
+                if (!await isSortedList(client, listData, normalizedLevels)) {
+                    await saveSortedList(listData.sha, normalizedLevels.map(level => level.name));
+                    await printChangelog(db, client, normalizedLevels, listData);
                 }
             }
         } catch (error) {
