93819003399e4e96340b8dccab10c2fe97c36d3a
Zukaritasu <perezjhonlenno29@gmail.com>
2025-10-13 18:12:22 -0400

    The leave and join functions are temporarily unavailable

--- src/commands/text-xp/user.js
+++ src/commands/text-xp/user.js
@@ -17,47 +17,115 @@
 
 const logger = require("../../logger")
 const topLimits = require("../../../.botconfig/top-limits.json")
-const { COLL_TEXT_XP } = require('../../../.botconfig/database-info.json') 
+const { COLL_TEXT_XP } = require('../../../.botconfig/database-info.json');
+const { Db } = require("mongodb");
+const { ChatInputCommandInteraction, GuildMember } = require("discord.js");
+const topxpBacklist = require('../text-commands/topxp-blacklist')
 
+/////////////////////////////////////////
 
+/**
+ * @typedef {Object} UserInfo
+ * @property {string} id
+ * @property {number} position
+ * @property {number} xp
+ * @property {boolean} assigned
+ */
+
+/**
+ * @param {Db} database 
+ * @param {ChatInputCommandInteraction} user 
+ * @returns {Promise<UserInfo | undefined>}
+ */
+async function getUserInfo(database, interaction) {
+    const top_xp = await database.collection(COLL_TEXT_XP).findOne(
+        {
+            type: 'userslist'
+        });
+
+    if (!top_xp || !('userslist' in top_xp))
+        return undefined;
+    return top_xp.userslist.find(user => user.id === interaction.member.id)
+}
+
+/**
+ * 
+ * @param {Db} database 
+ * @param {ChatInputCommandInteraction} interaction 
+ */
 async function leave(database, interaction) {
-	await interaction.deferReply({ ephemeral: true })
-    await interaction.editReply('Esta función está deshabilitada temporalmente...')
-}
+    /*try {
+        if (!interaction.member) return
 
-async function join(database, interaction) {
-	await interaction.deferReply({ ephemeral: true })
-    await interaction.editReply('Esta función está deshabilitada temporalmente...')
-}
-
-async function position(database, interaction) {
-    try {
         await interaction.deferReply({ ephemeral: true })
-        const top_xp = await database.collection(COLL_TEXT_XP).findOne(
-            {
-                type: 'userslist'
-            });
-    
-        if (!top_xp || !('userslist' in top_xp))
-            return await interaction.editReply('La lista de usuarios no está disponible <:ani_okitathinking:1244840221376512021>')
-        const user = top_xp.userslist.find(user => user.id === interaction.member.id)
-        if (!user)
+
+        const member = interaction.member
+        const userInfo = await getUserInfo(database, interaction)
+        if (!userInfo)
             return await interaction.editReply(`Tu posición no existe dentro del Top ${topLimits.limit} <:ani_chibiqiqipeek:1244839483581403138>`)
-        return await interaction.editReply(`Tu posición actual es **${user.position}** <:steamunga:1298001230790135939>`)
+        if (!member.roles.cache.has(topLimits.starsRoleID))
+            return await interaction.editReply(`No tienes asignado el rol Estrellas`)
+        await member.roles.remove(topLimits.starsRoleID, `The user unsubscribed with the Estrellas role`)
+        await topxpBacklist.addUser(member.id, database, interaction, false, true)
+        await interaction.editReply(`Se ha procesado la solicitud con éxito!`)
     } catch (error) {
         logger.ERR(error)
         try {
-            await interaction.editReply({ 
-                content: 'Ups! Ha ocurrido un error. Intenta mas tarde... <:birthday2:1249345278566465617>' 
+            await interaction.editReply({
+                content: 'Ups! Ha ocurrido un error. Intenta mas tarde... <:birthday2:1249345278566465617>'
+            })
+        } catch (replyError) {
+            logger.ERR(replyError)
+        }
+    }*/
+}
+
+/**
+ * 
+ * @param {Db} database 
+ * @param {ChatInputCommandInteraction} interaction 
+ */
+async function join(database, interaction) {
+    /*try {
+        if (!interaction.member) return
+        await interaction.deferReply({ ephemeral: true })
+
+        const member = interaction.member
+        if (!await topxpBacklist.isUserBlacklisted(member.id, database)) {
+            return await interaction.editReply(`Se ha procesado la solicitud con éxito!`)
+        }
+    } catch (error) {
+        
+    }*/
+}
+
+/**
+ * 
+ * @param {Db} database 
+ * @param {ChatInputCommandInteraction} interaction 
+ * @returns 
+ */
+async function position(database, interaction) {
+    try {
+        await interaction.deferReply({ ephemeral: true })
+        const userInfo = await getUserInfo(database, interaction)
+        if (!userInfo)
+            return await interaction.editReply(`Tu posición no existe dentro del Top ${topLimits.limit} <:ani_chibiqiqipeek:1244839483581403138>`)
+        return await interaction.editReply(`Tu posición actual es **${userInfo.position}** <:steamunga:1298001230790135939>`)
+    } catch (error) {
+        logger.ERR(error)
+        try {
+            await interaction.editReply({
+                content: 'Ups! Ha ocurrido un error. Intenta mas tarde... <:birthday2:1249345278566465617>'
             })
         } catch {
-            
+
         }
     }
 }
 
 module.exports = {
-	leave,
-	join,
-	position
+    leave,
+    join,
+    position
 }
