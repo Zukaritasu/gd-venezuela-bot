f407d082a80b258a9d6e291a4a8bc655a63c01a7
Zukaritasu <perezjhonlenno29@gmail.com>
2026-07-20 10:51:38 -0400

    fix: update copyright years in systeminfo and messageCreate files

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2024 Zukaritasu
+ * Copyright (C) 2024 - 2026 Zukaritasu
  * 
  * his program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
