d86c59a8a6e8036397fc27d3709a74d68dccf8f2
Zukaritasu <perezjhonlenno29@gmail.com>
2026-08-09 08:59:13 -0400

    fix: Update event name to MessageDelete and improve license header

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -1,17 +1,35 @@
-const { Events, Client, ChatInputCommandInteraction, Message, GuildMember, AttachmentBuilder, ChannelType } = require('discord.js');
+/**
+ * Copyright (C) 2026 Zukaritasu
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const { Events, Client, ChatInputCommandInteraction, Message,
+	GuildMember, AttachmentBuilder, ChannelType } = require('discord.js');
 const { Db } = require('mongodb');
 const logger = require('../logger');
 
 module.exports = {
-    name: Events.MessageCreate,
-    once: false,
+	name: Events.MessageDelete,
+	once: false,
 
 	/**
 	 * @param {Client} _client
 	 * @param {Db} _database
 	 * @param {Message} message
 	 */
-	async execute(_client, _database, message) {	
+	async execute(_client, _database, message) {
 		try {
 			if (message.author.bot || message.channel.id !== '1272033491390828574')
 				return;
